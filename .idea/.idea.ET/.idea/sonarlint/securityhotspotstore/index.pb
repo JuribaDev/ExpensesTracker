fc4f90ad418fd9de994cf15db93334f324a6b613
Juriba <Juribaalsiari@gmail.com>
2023-12-06 23:47:53 +0300

    initial commit

---
+++ .idea/.idea.ET/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,3 @@
+
+R
+"ExpensesTracker/Protos/greet.proto,c/e/ce05230e3d7c723a5ba26866cb7b645748aab1ee
---
+++ ExpensesTrackerTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace ExpensesTrackerTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
